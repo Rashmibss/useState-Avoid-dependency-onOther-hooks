6857ba8eb305e23d78b74257381da7d039b2cdff
rashmi1120 <rashmi.bsubash@gmail.com>
2022-01-04 11:12:15 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from "react";
+import "./styles.css";
+
+export default function App() {
+  const [phoneColor, setPhoneColor] = useState("black");
+  const [headsetColor, setHeadSetColor] = useState("red");
+
+  // add dependencies to prevent eslint warning
+  useEffect(() => {
+    setPhoneColor("red");
+  }, []);
+
+  // Runs the callback only on mount
+  useEffect(() => {
+    setHeadSetColor("black");
+  }, [setHeadSetColor]);
+
+  return (
+    <div className="app">
+      <h1>Multiple useState Hooks</h1>
+      <div>
+        <h3 style={{ color: phoneColor }}>
+          I am planning to buy a {phoneColor} colored iPhone
+        </h3>
+        <h3 style={{ color: headsetColor }}>
+          I am planning to buy a {headsetColor} colored headset
+        </h3>
+      </div>
+    </div>
+  );
+}
