0732cbd367deb7de8dc20ea6b145a2cad4f9328a
rashmi1120 <rashmi.bsubash@gmail.com>
2022-01-11 10:20:42 +0530

    added flexbox and enhanced the style

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ export default function App() {
   return (
     <div className="app">
       <h1>Multiple useState Hooks</h1>
-      <div className="info">
+      {/* <div className="info">
         There are two hooks here =&gt;
         <p>1. To update the color of the phone color</p>
         <p>2. To update the color of the headset</p>
@@ -36,6 +36,40 @@ export default function App() {
         <h3 style={{ color: headsetColor }}>
           I am planning to buy a {headsetColor} colored headset
         </h3>
+      </div> */}
+
+      <div class="flex-container">
+        <div class="flex-child magenta">
+          <h3>Update the color of the phone color</h3>
+
+          <div className="images-container">
+            <h4>Snapshot of how it is being updated</h4>
+            <img src={useffectWithoutDep} alt="Dep" />
+          </div>
+
+          <div className="container">
+            <h4>Output</h4>
+            <p style={{ color: phoneColor }}>
+              I am planning to buy a {phoneColor} colored iPhone
+            </p>
+          </div>
+        </div>
+
+        <div class="flex-child green">
+          <h3>To update the color of the headset</h3>
+
+          <div className="images-container">
+            <h4>Snapshot of how it is being updated</h4>
+            <img src={useEffectWithDep} alt="Dep" />
+          </div>
+
+          <div className="container">
+            <h4>Output</h4>
+            <p style={{ color: headsetColor }}>
+              I am planning to buy a {headsetColor} colored headset
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
